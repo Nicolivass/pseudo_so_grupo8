0fa300df5fb4a67bb586f0c1dd8c2a97bd0b7e90
Nicole <nicjlucasn@gmail.com>
2025-07-12 11:38:52 -0300

    scope

--- utils/parser.py
+++ utils/parser.py
@@ -1,1 +1,9 @@
 #Funções parse_processos() e parse_arquivos().
+
+def parse_processos(caminho):
+    # Lê processos.txt e retorna lista de objetos Processo
+    pass
+
+def parse_arquivos(caminho):
+    # Lê arquivos.txt e retorna lista de operações e estado do disco
+    pass
